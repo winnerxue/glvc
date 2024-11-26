45bf475edec2e0ee7dad532d03ad07f76643aa0e
xuezhang <winnerxue_mail163@163.com>
2024-11-27 00:35:36 +0800

    fix

---
+++ strings/builder.go
@@ -0,0 +1,24 @@
+package strings
+
+import "bytes"
+
+// 定义一个 Builder 类型，模仿 strings.Builder 的接口
+type Builder struct {
+	buf bytes.Buffer
+}
+
+func (sb *Builder) WriteString(s string) (int, error) {
+	return sb.buf.WriteString(s)
+}
+
+func (sb *Builder) WriteByte(c byte) error {
+	return sb.buf.WriteByte(c)
+}
+
+func (sb *Builder) WriteRune(r rune) (int, error) {
+	return sb.buf.WriteRune(r)
+}
+
+func (sb *Builder) String() string {
+	return sb.buf.String()
+}
